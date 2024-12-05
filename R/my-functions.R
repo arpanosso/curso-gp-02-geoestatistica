f7aeddbefa3ee65f0cd5bcfc8525b94d3242c07e
Alan Rodrigo Panosso <arpanosso@yahoo.com.br>
2024-12-04 22:41:12 -0300

    nova aula

--- R/my-functions.R
+++ R/my-functions.R
@@ -13,3 +13,33 @@ create_lag <- function(column, lag){
   nvec <- c(rep(NA,lag),column[1:(n-lag)])
   return(nvec)
 }
+
+# parametros do semivariograma
+get_mod_par_stat <- function(.variogram, .mod){
+  modelo <-as.character(.mod$model[[2]])
+  sse <- attr(.mod, "SSErr") # soma quadrados resíduos
+  nugget <- .mod$psill[1] # efeito pepita C0
+  sill <- sum(.mod$psill) # patamar C0 + C1
+  range <- .mod$range[2] # alcance
+  dist <- .variogram$dist
+  gamma <- .variogram$gamma
+  rs_total <- (gamma - mean(gamma))^2
+  if(modelo == "Sph"){
+    gamma_pred <- ifelse(dist <= range, nugget + (sill-nugget)*(3/2*(dist/range)-1/2*(dist/range)^3),sill)
+  }
+  if(modelo == "Exp"){
+    gamma_pred <- nugget + (sill-nugget)*(1-exp(-3*(dist/(range*3))))
+  }
+  if(modelo == "Gau"){
+    gamma_pred <- nugget + (sill-nugget)*(1-exp(-3*(dist/(range*(3^.5)))^2))
+  }
+
+  rs_mod <- (gamma - gamma_pred)^2
+  r2 <- (sum(rs_total)-sum(rs_mod))/sum(rs_total)
+
+  vout <- paste0(c("model","c0","c0+c1","a","sse","r2"),
+                 " = ",
+                 c(modelo, round(c(nugget,sill,range,sse,r2),3))
+  )
+  return(vout)
+}
--- scripts/analise-geoestatistica.R
+++ scripts/analise-geoestatistica.R
@@ -7,7 +7,7 @@ library(tidyverse)
 library(sp)
 library(gstat)
 library(corrplot)
-source("../R/my-functions.R")
+source("R/my-functions.R")
 
 ## Ler o banco de dados
 dados_geo <- read_rds("data/geo_fco2.rds") # lenitura
@@ -56,7 +56,9 @@ dados_geo_eu |>
     CV = 100*Desvio_Padrao/Media
   ) |> t()
 
-## VOLTEMOS À APRESENTAÇÃO
+############################
+## VOLTEMOS À APRESENTAÇÃO #
+############################
 ## Crie o histograma da variável `mo`, adicionando
 # adicionando os valores de `média`, `mediana`,
 # `primeiro` e `terceiro quartil`.
@@ -99,8 +101,9 @@ dados_geo_eu |>
   lm(formula = mo ~ y) |>
   summary.lm()
 
-## VOLTEMOS À APRESENTAÇÃO
-
+############################
+## VOLTEMOS À APRESENTAÇÃO #
+############################
 ## Crie a matriz de correlação linear entre todas as variáveis
 # numéricas do banco de dados.
 dados_geo_eu |>
@@ -113,18 +116,8 @@ dados_geo_eu |>
   corrplot(method = "ellipse",
            type = "upper")
 
-## Crie o gráfico de dispersão para MO com ela mesma, no lag 1
-# cada lag equivale a 50 m.
-transecto <- read_rds("../data/transecto.rds")
-glimpse(transecto)
-transecto |>
-  filter( y == 0) |>
-  ggplot(aes(x=x,y=arg)) +
-  geom_line() +
-  theme_bw()
-
 ## Para o estudo da autocorrelação vamos carregar um banco de dados
-# sobre a textura do solo com pontos dispostes em transectos
+# sobre a textura do solo com pontos dispostos em transectos
 # de 50 m de distância de separação entre eles.
 transecto <- read_rds("data/transecto.rds")
 glimpse(transecto)
@@ -134,7 +127,7 @@ transecto |>
   geom_line() +
   theme_bw()
 
-## Para exemplificar, vamos olhar a matriz decorrelação contruída
+## Para exemplificar, vamos olhar a matriz de correlação contruída
 # a partir da variável argila com ela mesma em diverentes lags.
 transecto |>
   filter( y == 0) |>
@@ -154,3 +147,149 @@ transecto |>
   select(-x) |>
   cor() |>
   corrplot(type = "upper")
+
+## Vamos construir um semivariograma experimental:
+# Inicialmente vamos extrair os vetores `x`, `y` e `z`,
+# onde z será nossa variáviel MO.
+x <- dados_geo_eu |>  pull(x)
+y <- dados_geo_eu |>  pull(y)
+z <- dados_geo_eu |>  pull(mo)
+
+## Devemos agora construir as matrizes de distância e a
+# matriz de valor de gamma (variância).
+matriz_dist <- matrix(0,ncol=102,nrow=102)
+matriz_gamma <- matrix(0,ncol=102,nrow=102)
+
+
+## Calcular a matriz de distância entre todos os pontos amostrais
+# e a matriz de valores de gamma.
+for(i in 1:102){
+  for(j in 1:102){
+    matriz_dist[i,j] = sqrt((x[i]-x[j])^2 + (y[i]-y[j])^2)
+    matriz_gamma[i,j] = (z[i]-z[j])^2
+  }
+}
+
+
+table(matriz_dist) %/% 2 |>
+  plot()
+abline(h=30, col="red", lty =2)
+# E recomenda-se um número de pares de ponto em uma dada
+# distância de separação h sempre maior que 30.
+
+## Calcule h, N_h e gamma.
+n_lags <- 5
+active_lag_distance <- 60
+h <- seq(5, active_lag_distance, n_lags)
+n_h <- h
+gamma_h <- h
+for(i in seq_along(h)){
+  if(i == 1){
+    filtro <- matriz_dist > 0 & matriz_dist <= h[i]
+  }else{
+    filtro <- matriz_dist > h[i-1] & matriz_dist <= h[i]
+  }
+  n_h[i] <- sum(filtro)/2
+  gamma_h [i] <- sum(matriz_gamma[filtro])/2/n_h[i]/2
+}
+
+## Construa o semivariograma experimental h vs gamma.
+tibble(n_h,h,gamma_h) |>
+  ggplot(aes(x=h,y=gamma_h)) +
+  geom_point(size=3) +
+  theme_bw()
+
+## Construção do semivariograma pela função variogram do
+# pacote gstat
+# Recorte do banco de dados com x, y, e z
+df_aux <- dados_geo_eu |>
+  select(x, y, mo) |>
+  rename(z = mo)
+class(df_aux)
+
+## Deve-se passar df_aux para SpatialData uma vez que
+# as funções do gstat trabalham com esse formato de objeto
+coordinates(df_aux) = ~ x + y
+head(df_aux)
+class(df_aux)
+
+# criando a fórmula para o semivariograma
+form <- z ~ 1
+
+# criar o semivarigrama experimental
+semi_exp <- variogram(form, df_aux,
+                             cutoff=60,
+                             width = 5)
+semi_exp |>
+  ggplot(aes(x=dist, y=gamma)) +
+  geom_point(size=3) +
+  theme_bw() +
+  annotate("text",
+           x=semi_exp$dist,
+           y=semi_exp$gamma*0.98,
+           label = semi_exp$np)
+
+## Modelagem - Modelo Esférico
+mod_ajust <- fit.variogram(semi_exp,
+                           vgm(model = "Sph",
+                               nugget=15,
+                               psill=30,
+                               range=20)
+)
+texto_par <- get_mod_par_stat(semi_exp,mod_ajust)
+semi_exp |>
+  ggplot(aes(x=dist, y=gamma)) +
+  geom_point(size=3) +
+  theme_bw()+
+  geom_line(data = variogramLine(mod_ajust,
+                                 maxdist = 60),
+            aes(x=dist, y=gamma),
+            color="red") +
+  annotate("text",x=50,
+           y=seq(22.5,10,-2.5),
+           label = texto_par,size=6)
+
+
+## Crie a região de adensamento, ou seja, um novo arquivo
+# contendo a posição geográfica (pares de coordenadas) onde
+# será realizada a estimativa via krigagem ordinária.
+dados_geo_eu |>
+  ggplot(aes(x=x,y=y)) +
+  geom_point() +
+  theme_bw()
+
+menor_dist <- 1
+grid_adensado <- expand.grid(
+  x=seq(0,100,menor_dist),
+  y=seq(0,100,menor_dist)
+)
+
+dados_geo_eu |>
+  ggplot(aes(x=x,y=y)) +
+  geom_point() +
+  geom_point( data = grid_adensado,
+              aes(x=x,y=y),
+              color="blue",
+              size=1) +
+  theme_bw()
+
+## transformar o grid_adensado em um objeto
+# SpatiaData
+coordinates(grid_adensado) = ~ x+ y
+
+## Utilizando o algoritmo da KO, vamos estimar o atributo nos
+# locais não amostrados (gradeado adensado).
+ko <- gstat::krige(form, df_aux,grid_adensado,
+                   mod_ajust,
+                   block=c(0.1,0.1))
+
+## Apresente os padrões espaciais (mapa de ko) e o mapa
+# da estimativa do erro.
+ko |>
+  as_tibble() |>
+  ggplot(aes(x=x,y=y)) +
+  geom_tile(aes(fill= var1.pred)) +
+  scale_fill_viridis_c() +
+  labs(title = "FCO2") +
+  theme_bw() +
+  labs(fill="MO")
